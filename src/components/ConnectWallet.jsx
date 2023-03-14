3126fa1ab8cd89c7cbc220c90084c1550bef3a96
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-03-13 20:52:46 -0400

    chain id

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -3,8 +3,11 @@ import DarkModeSwitch from "./DarkModeSwitch"
 import {useRouter} from "next/navigation"
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { networks } from "@/app/utils/networks";
+
 
 import db, {addUserToDb, checkDbForUser} from "@/app/utils/firestore";
+import { ethers } from "ethers";
 
 
 
@@ -13,6 +16,8 @@ import db, {addUserToDb, checkDbForUser} from "@/app/utils/firestore";
 const ConnectWallet = () => {
     const router = useRouter()
     const [activeAccount, setActiveAccount] = useState()
+
+    const [network, setNetwork] = useState("")
 
 
 
@@ -24,6 +29,9 @@ const ConnectWallet = () => {
                 return;
             }
             const accounts = await ethereum.request({method:"eth_requestAccounts"})
+
+
+            
             if(accounts.length !== 0 ){
                 setActiveAccount(accounts[0]);
                 
@@ -56,6 +64,17 @@ const ConnectWallet = () => {
                 router.push(`/`)
             }
             const accounts = await ethereum.request({method: "eth_accounts"})
+            const chainId = await ethereum.request({method: "eth_chainId"})
+            console.log(chainId)
+
+            setNetwork(networks[chainId])
+
+            ethereum.on('chainChanged', handleChainChanged);
+
+            function handleChainChanged(_chainId) {
+                window.location.reload();
+            }
+            
             if(accounts.length !== 0 ){
                 setActiveAccount(accounts[0]);
                 
@@ -71,6 +90,27 @@ const ConnectWallet = () => {
         }
     }
 
+    const handleNetworkSwitch = async () =>{
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: "0x3"}],
+            });
+            console.log("You have switched to the right network")
+            
+          } catch (switchError) {
+            
+            // The network has not been added to MetaMask
+            if (switchError.code === 4902) {
+             console.log("Please add the Polygon network to MetaMask")
+            }
+            console.log("Cannot switch to the network", switchError)
+            
+          }
+    }
+    
     useEffect(()=>{
         checkIfWalletIsConnected()
     },[])
@@ -87,7 +127,8 @@ const ConnectWallet = () => {
         
     <div className='flex justify-end mt-6 mr-10 space-x-4 '>
         {!activeAccount ? <button className="w-48 transition-shadow bg-gray-400 rounded-full hover:underline hover:shadow-lg" onClick={connectWallet}><span className="">Connect Wallet</span> </button> : 
-        (
+        (network !== "Hardhat Testnet" ?  <button onClick={handleNetworkSwitch} className="transition-shadow hover:underline hover:shadow">Switch Network</button> :
+
             <>
             <Link href={`/chat/profile`} className="transition-shadow hover:underline hover:shadow">Profile</Link>
             <Link href={`/chat/profile`} className="transition-shadow hover:underline hover:shadow">{activeAccount.slice(0,4)}...{activeAccount.slice(-6)}</Link>
